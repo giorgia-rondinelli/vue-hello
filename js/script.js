542911ccf7a0b4e62953fecebdc34baf2986cf7a
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-03-11 21:26:59 +0100

    [add] countdown

--- js/script.js
+++ js/script.js
@@ -2,8 +2,35 @@ const{createApp}=Vue
 createApp({
   data(){
     return{
-      messaggio:'ciao!'
+      messaggio:'ciao!',
+      countdown: 10,
+      stop: 'fine'
 
     }
-  }
+  },
+  methods:{
+    startCountdown(){
+      setInterval(() =>
+       {  
+        if(this.countdown<=0){
+          this.countdown= 0
+        }
+        
+        else{
+          this.countdown--
+        }
+       
+      },1000)}
+
+   
+  , 
+    resetCountdown(){
+      this.countdown=10
+
+    }
+},
+mounted(){
+  
+  
+}
 }).mount('#app')
